95b34eccc4a0b20f3d8d68855f3d2bf853d4505c
LordCracken <vladislav070201@gmail.com>
2022-12-12 02:51:34 +0300

    add loading comments by click on button

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,4 @@
-import { Route, useParams } from 'react-router-dom';
+import { Route, useParams, Link } from 'react-router-dom';
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
@@ -18,6 +18,13 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote author={quote.author} text={quote.text} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
