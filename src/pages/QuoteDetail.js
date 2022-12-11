f97872b34e47dac6a901d2114df31da6943b4d6d
LordCracken <vladislav070201@gmail.com>
2022-12-11 23:40:10 +0300

    add info from data to quote detail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,23 @@
 import { Route, useParams } from 'react-router-dom';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Vlad', text: 'Learning React is great!' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) return <p>No quote found!</p>;
+
   return (
     <>
-      <h1>Quotes Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote author={quote.author} text={quote.text} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
